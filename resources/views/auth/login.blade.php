89f60efbb082d4da62746a6b33a2785739bdcf27
zhaozhentao <344696734@qq.com>
2017-04-24 09:05:15 +0800

    ui修改

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-4 col-md-offset-4 floating-box">
+        <div class="col-md-4 col-md-offset-4">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h3 class="panel-title">登录</h3>
--- resources/views/topics/partials/body.blade.php
+++ resources/views/topics/partials/body.blade.php
@@ -1,11 +1,16 @@
 <div class="markdown-body" id="emojify">
-    {!! $body !!}
+  {!! $body !!}
 
-    @if($topic->user->signature)
-        <a class="popover-with-html" data-content="作者署名" target="_blank"
-           style="display: block;width: 30px;color: #ccc;margin: 22px 0 8px;"
-           href="https://laravel-china.org/topics/3422"><i class="fa fa-paw" aria-hidden="true"></i></a>
-        {!! $topic->user->present()->formattedSignature() !!}
-    @endif
+  @if($topic->user->signature)
+    <div class="signature">
+      <a class="popover-with-html" data-content="作者署名" target="_blank"
+         style="color: #ccc;" href="https://laravel-china.org/topics/3422">
+        <i class="fa fa-paw" aria-hidden="true"></i>
+        <span style="display: inline-block; margin-left: 8px;">
+          {!! $topic->user->present()->formattedSignature() !!}
+        </span>
+      </a>
+    </div>
+  @endif
 
 </div>
